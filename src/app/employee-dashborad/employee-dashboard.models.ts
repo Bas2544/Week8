af7631a0aa20a3e276129d02cc2c4fc9932a908c
Bas2544 <thebasza.2544@gmail.com>
2021-12-21 17:27:54 +0700

    first commit

---
+++ src/app/employee-dashborad/employee-dashboard.models.ts
@@ -0,0 +1,8 @@
+export class EmployeeModels{
+    id : number = 0;
+    firstname: string = '';
+    lastName: string = '';
+    email: string = '';
+    telephone: string = '';
+    salary: number = 0
+}
